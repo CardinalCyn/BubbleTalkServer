5bcb5263e39d9d90bd965cf8be51df0a42fb9cc0
Salah Zanabili <39255304+CardinalCyn@users.noreply.github.com>
2023-06-12 17:29:56 -0500

    Update db.js

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const filterRoomName=filterRoom.filterRoomName;
 const config={
     connectionLimit:process.env.DB_CONNECTION_LIMIT,
     host:process.env.DB_HOST,
+    port:process.env.PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE
