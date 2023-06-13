739c6bfb622cc66cf3e5cf8453c00aaa48f5d4a3
Salah Zanabili <39255304+CardinalCyn@users.noreply.github.com>
2023-06-12 22:46:43 -0500

    Update db.js

--- db.js
+++ db.js
@@ -5,11 +5,13 @@ const filterRoomName=filterRoom.filterRoomName;
 //configures
 const config={
     connectionLimit:process.env.DB_CONNECTION_LIMIT,
+    connectTimeout  : 60 * 1000,
+    acquireTimeout  : 60 * 1000,
+    timeout         : 60 * 1000,
     host:process.env.DB_HOST,
-    port:process.env.PORT,
+    port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
 }
 
 const db=mysql.createPool(config);
